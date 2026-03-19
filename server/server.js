2e3d2878fe37ad8cfddc19e4e1c9696de012b9e0
matthewromaniuk <matthewromaniukk@gmail.com>
2026-03-18 19:26:05 -0500

    added supabase api, updated server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-const express = require('express');
+import express from "express";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Define a route for GET requests to the root URL
-app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
-});
+app.get("/", (_, response) =>
+  response.json({ info: "Express app with Supabase" })
+);
 
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () =>
+  console.log(
+    new Date().toLocaleTimeString() + `: Server is running on port ${3000}...`
+  )
+);
